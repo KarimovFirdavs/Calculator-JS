a00a40da3647b28a4a49a34e613304ff87d1bce3
KarimovFirdavsFE[14] <115251742+KarimovFirdavs@users.noreply.github.com>
2022-10-10 20:25:23 +0500

    Add files via upload

---
+++ calculator JS/js/script.js
@@ -0,0 +1,20 @@
+let age = prompt('Kalendar');
+
+if (age == 1) {
+    console.log('Monday');
+} else if (age == 2) {
+    console.log('Tuesday');
+} else if (age == 3) {
+    console.log('Wendesday');
+} else if (age == 4) {
+    console.log('Thoursday');
+} else if (age == 5) {
+    console.log('Friday');
+} else if (age == 6) {
+    console.log('Saturday');
+} else if (age == 7) {
+    console.log('Sunday');
+} else {
+    console.log('AMOGUS');
+}
+
